9f39e6f0f65c07f233bd893fd23738394bbfe275
erick-sudo <erickocchieng766@gmail.com>
2024-01-28 00:10:40 +0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import {} from "react";
+import "./App.css";
+
+function App() {
+  return <div>
+    <h2 className="text-4xl font-extrabold text-center p-4">O-Foundation</h2>
+  </div>;
+}
+
+export default App;
